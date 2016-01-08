a71b4d8847a8cacee9653c84a4bfec71d643f5ae
Yusuke Nunokawa <yusuke_nunokawa@esrij.com>
2016-01-08 15:41:07 +0900

    add process.env.CALLBACK_URL

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ passport.deserializeUser(function(obj, done) {
 passport.use(new FacebookStrategy({
     clientID: FACEBOOK_APP_ID,
     clientSecret: FACEBOOK_APP_SECRET,
-    callbackURL: "http://localhost:3000/auth/facebook/callback"
+    callbackURL: process.env.CALLBACK_URL || "http://localhost:3000/auth/facebook/callback"
   },
   function(accessToken, refreshToken, profile, done) {
     // asynchronous verification, for effect...
